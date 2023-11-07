885c844e487dd173290111469717d3115a263dec
Balint Erdi <balint@balinterdi.com>
2023-11-07 23:06:08 +0100

    Add a v2 add-on as an in-repo one

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { Webpack } = require('@embroider/webpack');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
   });
 
-  return app.toTree();
+  return require('@embroider/compat').compatBuild(app, Webpack, {});
 };
