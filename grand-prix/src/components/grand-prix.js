fbd42f7f4776274f3a98ec2db6d870a5f3e3fd24
Balint Erdi <balint@balinterdi.com>
2023-11-09 18:35:49 +0100

    [WIP] Try pointing v2 addon srcDir into a top-level folder in the host app
    That mostly works with the exception of the image

--- grand-prix/src/components/grand-prix.js
+++ grand-prix/src/components/grand-prix.js
@@ -2,6 +2,10 @@ import Component from '@glimmer/component';
 import { getOwnConfig } from '@embroider/macros';
 
 export default class GrandPrixComponent extends Component {
+  get imageSrc() {
+    return '/grand-prix/grand-prix-attack.webp';
+  }
+
   get device() {
     return getOwnConfig().device;
   }
