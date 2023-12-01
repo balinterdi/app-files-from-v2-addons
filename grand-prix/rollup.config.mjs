5d303c0717268606b96932a49a5ad4225fd09097
Balint Erdi <balint@balinterdi.com>
2023-12-01 16:09:24 +0100

    [WIP] Try adding a route template to the host app

--- grand-prix/rollup.config.mjs
+++ grand-prix/rollup.config.mjs
@@ -1,14 +1,17 @@
 // import copy from 'rollup-plugin-copy';
 import babel from "@rollup/plugin-babel";
 import { Addon } from "@embroider/addon-dev/rollup";
+import copy from "rollup-plugin-copy";
 
 // eslint-disable-next-line no-undef
 const device = process.env.DEVICE;
 
 console.log(`Building for ${device}`);
 
+const sourceDir = `src/${device}`;
+
 const addon = new Addon({
-  srcDir: `src/${device}`,
+  srcDir: sourceDir,
   destDir: "dist",
 });
 
@@ -20,7 +23,11 @@ export default {
   plugins: [
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
-    addon.publicEntrypoints(["components/**/*.js", "router.js"]),
+    addon.publicEntrypoints([
+      "components/**/*.js",
+      "templates/**/*.hbs",
+      "router.js",
+    ]),
 
     // This seems to be needed for the build step to update `package.json` with
     // `public-assets` which is required for images to work.
@@ -31,7 +38,11 @@ export default {
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
-    addon.appReexports(["components/**/*.js", "router.js"]),
+    addon.appReexports([
+      "components/**/*.js",
+      "templates/**/*.hbs",
+      "router.js",
+    ]),
 
     babel({ babelHelpers: "bundled" }),
 
@@ -50,14 +61,13 @@ export default {
     // Remove leftover build artifacts when starting a new build.
     addon.clean(),
 
-    // Copy Readme and License into published package
-    /*
+    // Copy templates to the dist folder so they can be used by the app
+    // (Not sure if that's a sanctioned way, though)
     copy({
+      verbose: true,
       targets: [
-        { src: '../README.md', dest: '.' },
-        { src: '../LICENSE.md', dest: '.' },
+        { src: `${sourceDir}/templates/**/*.hbs`, dest: "dist/templates/" },
       ],
     }),
-    */
   ],
 };
